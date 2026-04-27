e30785e740f554a8fe98ec852c6c79af55eded49
BenSchwartz96 <117608074+BenSchwartz96@users.noreply.github.com>
2026-04-27 02:40:56 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import Header from '../components/header/Header'
+import Nav from '../components/nav/Nav'
+import About from '../components/about/About'
+import Details from '../components/details/Details'
+import Skills from '../components/skills/Skills'
+import Portfolio from '../components/portfolio/Portfolio'
+import Contact from '../components/contact/Contact'
+import Footer from '../components/footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Details />
+      <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
