49597cce32d7d4265492b071853566905abbd255
BenSchwartz96 <117608074+BenSchwartz96@users.noreply.github.com>
2026-05-25 18:17:03 +0100

    Added some subheaders

--- src/components/projects/agis/agis-header/AgisHeader.jsx
+++ src/components/projects/agis/agis-header/AgisHeader.jsx
@@ -9,6 +9,11 @@ const AgisHeader = () => {
           <h1>Agis</h1>
         </div>
 
+        <div className="agis__subheaders">
+          <h3>Learner Project PvE Positional Card Game</h3>
+          <h3>Focus: Learning the Basics</h3>
+        </div>
+
       </div>
     </header>
   )
